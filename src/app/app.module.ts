851e86cb5e6185554410004293c5b0a98b19933d
CobyBoy <aboutacoby@gmail.com>
2024-01-13 10:50:37 -0300

    Add animations to enter and leave

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
   declarations: [AppComponent, ToastComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+    BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { IToast } from '../toast.interface';
 import { ToastService } from '../services/toast.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
+  animations: [
+    trigger('messageState', [
+      state('*', style({
+        transform: 'translateY(0)', opacity: 1
+      })),
+      transition('void => *', [
+        style({transform: 'translateY(-100%)', opacity: 0}),
+        animate('0.5s ease-in')
+      ]),
+      transition('* => void', [
+        animate('2s', style({height: '0', opacity: 0})),
+        
+       
+      ]),
+    ]),
+  ]
 })
 export class ToastComponent implements OnInit {
   toasts: IToast[] = [];
